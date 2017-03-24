16202d584d1e93f5782f44cc2ddfd60a382ab5b4
Wicked-Dreamshaper <hamtylerhogg@hotmail.com>
2017-03-24 13:37:30 -0600

    Login Page

--- MEDIArchive/javaScript/loginpage.js
+++ MEDIArchive/javaScript/loginpage.js
@@ -1,6 +1,19 @@
 //loginpage.js
+var letters = "Monkeybustt_25".split('');
+var index = parseInt(0);
+var currentString = "";
 
 function login()
 {
-	window.location.href = "mainpage.html";
+	window.location.href = "mainPage.html";
+}
+
+function monkeybustt()
+{
+	if(index < 14)
+	{
+		currentString += letters[index];
+		index += 1;
+	}
+	document.getElementById('output').value = currentString;
 }
